df74ade97b73b7e318f34b94703a00268ded4ce5
Kuneosu <pineone@pineoneui-MacBookPro.local>
2024-03-15 11:09:06 +0900

    Make Navigation, Make 3 Fragment

--- app/src/main/java/com/kks/trot_list/MainActivity.kt
+++ app/src/main/java/com/kks/trot_list/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1. navigation 만들기
+
+        // 2. Fragment 만들고 네비게이션에 연결하기
+
+        // 3. 리사이클러뷰 연결하기
     }
 }
