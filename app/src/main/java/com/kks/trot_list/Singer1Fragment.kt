6b31fba72bcbaf0291a10ec41c1a01eb53295c01
Kuneosu <kks92155784@gmail.com>
2024-03-15 14:16:44 +0900

    프래그먼트와 네비게이션 연결, 데이터 바인딩 사용

--- app/src/main/java/com/kks/trot_list/Singer1Fragment.kt
+++ app/src/main/java/com/kks/trot_list/Singer1Fragment.kt
@@ -1,24 +1,45 @@
 package com.kks.trot_list
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.kks.trot_list.databinding.FragmentSinger1Binding
 
 
 class Singer1Fragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val TAG = "Singer1Fragment"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_singer1, container, false)
+
+        val binding: FragmentSinger1Binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_singer1, container, false)
+
+        binding.image2.setOnClickListener {
+            moveSinger2Fragment(it)
+        }
+        binding.image3.setOnClickListener {
+            moveSinger3Fragment(it)
+        }
+        return binding.root
     }
+
+    private fun moveSinger2Fragment(view: View) {
+        view.findNavController().navigate(R.id.action_singer1Fragment_to_singer2Fragment)
+    }
+
+    private fun moveSinger3Fragment(view: View) {
+        view.findNavController().navigate(R.id.action_singer1Fragment_to_singer3Fragment)
+    }
+
 }
--- app/src/main/java/com/kks/trot_list/Singer3Fragment.kt
+++ app/src/main/java/com/kks/trot_list/Singer3Fragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.kks.trot_list.databinding.FragmentSinger3Binding
 
 class Singer3Fragment : Fragment() {
 
-
+    private lateinit var binding: FragmentSinger3Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,6 +21,23 @@ class Singer3Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_singer3, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_singer3, container, false)
+        binding.image1.setOnClickListener {
+            moveSinger1Fragment(it)
+        }
+        binding.image2.setOnClickListener {
+            moveSinger2Fragment(it)
+        }
+
+        return binding.root
+    }
+
+    private fun moveSinger1Fragment(view: View) {
+        view.findNavController().navigate(R.id.action_singer3Fragment_to_singer1Fragment)
+    }
+
+    private fun moveSinger2Fragment(view: View) {
+        view.findNavController().navigate(R.id.action_singer3Fragment_to_singer2Fragment)
     }
 }
